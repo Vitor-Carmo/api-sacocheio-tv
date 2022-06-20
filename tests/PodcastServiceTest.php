dfee0ec857b7fffb9356242dd686fabb0047503b
Vitor Carmo <vitorv0071@gmail.com>
2022-06-19 23:29:13 -0300

    add some integrations tests

--- tests/PodcastServiceTest.php
+++ tests/PodcastServiceTest.php
@@ -9,13 +9,15 @@ use PHPUnit\Framework\TestCase;
 
 class PodcastServiceTest extends TestCase
 {
-  private function getUser(){
+  private function getUser()
+  {
     $user = User::login(\LOGIN, \PASSWORD);
     $user = json_decode($user);
     return $user;
   }
 
-  public function testShouldListAllPodcasts() {
+  public function testShouldListAllPodcasts()
+  {
     $user = $this->getUser();
     $token = $user->token;
     $this->assertIsString($token);
@@ -31,7 +33,8 @@ class PodcastServiceTest extends TestCase
     $this->assertGreaterThan(0, count($podcasts->data));
   }
 
-  public function testShouldGiveAnErrorBecauseIDidntPassTheToken() {
+  public function testShouldGiveAnErrorBecauseIDidntPassTheTokenOnListAllPodcasts()
+  {
     $podcasts = Api::get(\LOCAL_BASE_URL_API . "podcast/podcasts");
     $podcasts = json_decode($podcasts);
 
@@ -41,7 +44,7 @@ class PodcastServiceTest extends TestCase
     $this->assertIsObject($podcasts->data);
     $this->assertObjectHasAttribute('error', $podcasts->data);
   }
-    
+
   public function testShouldGetPodcastEpisode()
   {
     $episode_code = 945;
@@ -71,5 +74,59 @@ class PodcastServiceTest extends TestCase
     $this->assertObjectHasAttribute('data', $episode);
     $this->assertIsObject($episode->data);
     $this->assertObjectHasAttribute('error', $episode->data);
+  }
+
+  public function testShouldGetThePodcastAndItsEpisodesByName()
+  {
+    $name = "desinformação";
+
+    $podcast = Api::get(\LOCAL_BASE_URL_API . "podcast/podcast/$name");
+    $podcast = json_decode($podcast);
+
+    $this->assertIsObject($podcast);
+    $this->assertObjectHasAttribute('status', $podcast);
+    $this->assertSame($podcast->status, true);
+    $this->assertObjectHasAttribute('data', $podcast);
+    $this->assertIsObject($podcast->data);
+    $this->assertObjectHasAttribute('episodes', $podcast->data);
+    $this->assertIsObject($podcast->data->episodes);
+    $this->assertObjectHasAttribute('data', $podcast->data->episodes);
+    $this->assertIsArray($podcast->data->episodes->data);
+    $this->assertObjectHasAttribute('length', $podcast->data->episodes);
+    $this->assertIsInt($podcast->data->episodes->length);
+    $this->assertSame($podcast->data->episodes->length, count($podcast->data->episodes->data));
+  }
+
+  public function testShouldGiveAnErrorWhenTryingToGetPodcastWithAnEmptyName()
+  {
+    $name = "";
+    $podcast = Api::get(\LOCAL_BASE_URL_API . "podcast/podcast/$name");
+    $podcast = json_decode($podcast);
+
+    $this->assertIsObject($podcast);
+    $this->assertObjectHasAttribute('data', $podcast);
+    $this->assertIsObject($podcast->data);
+    $this->assertObjectHasAttribute('error', $podcast->data);
+  }
+
+  public function testShouldGetTheLatestEpisodeFromEachPodcast()
+  {
+    $user = $this->getUser();
+    $token = $user->token;
+    $this->assertIsString($token);
+
+    $podcasts = Api::get(\LOCAL_BASE_URL_API . "podcast/latest_episodes_of_each_podcast", $token);
+    $podcasts = json_decode($podcasts);
+
+    $this->assertIsObject($podcasts);
+    $this->assertObjectHasAttribute('status', $podcasts);
+    $this->assertSame($podcasts->status, true);
+    $this->assertObjectHasAttribute('data', $podcasts);
+    $this->assertIsArray($podcasts->data);
+    $this->assertGreaterThan(0, count($podcasts->data));
+
+    foreach ($podcasts->data as $podcast) {
+      $this->assertObjectHasAttribute('latest_episode', $podcast);
+    }
   }
 }
