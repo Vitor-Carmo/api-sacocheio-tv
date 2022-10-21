294b2626c3fa59f4704027c3d25aedadfecf5556
vitor-carmo <vitorv0071@gmail.com>
2022-10-20 22:15:56 -0300

    feat: add cache to podcasts

--- App/Models/Podcast.php
+++ App/Models/Podcast.php
@@ -4,40 +4,51 @@ namespace App\Models;
 
 class Podcast
 {
+
   public static function All($token)
   {
-    $data = Api::get(\SACOCHEIO_API_BASE_URL . 'podcasts', $token);
-    return json_decode($data);
+    return self::api_get_with_cache("programas", 'podcasts', $token);
   }
 
   public static function find($programa)
   {
-    $data = Api::get(\SACOCHEIO_API_BASE_URL . "podcasts/$programa");
-    return json_decode($data);
+    return self::api_get_with_cache($programa, "podcasts/$programa");
   }
 
   public static function episodes($id)
   {
-    $data = Api::get(\SACOCHEIO_API_BASE_URL . "episodios/getById/$id");
-    return json_decode($data);
+    return self::api_get_with_cache("episodios-$id", "episodios/getById/$id");
   }
 
 
-  public static function episode($code)
+  public static function episode($slug)
   {
-    $data = Api::get(\SACOCHEIO_API_BASE_URL . "episodios/getBySlug/$code");
-    return json_decode($data);
+    return self::api_get_with_cache("$slug",  "episodios/getBySlug/$slug");
   }
 
   public static function comments($code, $token)
   {
-    $data = Api::get(\SACOCHEIO_API_BASE_URL . "comments/$code", $token);
-    return json_decode($data);
+    return self::api_get_with_cache("comments-$code",  "comments/$code", $token);
   }
 
   public static function favorites($token)
   {
-    $data = Api::get(\SACOCHEIO_API_BASE_URL . "episodios/getFavoritos", $token);
-    return json_decode($data);
+    return self::api_get_with_cache("favoritos-$token", "episodios/getFavoritos", $token);
+  }
+
+
+  private static function api_get_with_cache($file_name, $url, $token = null)
+  {
+    if (!Cache::exist($file_name)) {
+      $data = Api::get(\SACOCHEIO_API_BASE_URL . $url, $token);
+      Cache::create($file_name, $data);
+      return json_decode($data);
+    }
+
+    $cache = json_decode(Cache::get($file_name));
+
+    Cache::check_if_cache_expired($file_name);
+
+    return $cache;
   }
 }
